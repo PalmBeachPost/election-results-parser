81e4ca275bc7a1c3d311c9e4f7f5dbb8834c3efe
unknown <stucka@whitedoggies.com>
2018-11-02 13:20:37 -0400

    Begin scripting the scripting of the scripting.

---
+++ onescripttorunthemall.py
@@ -0,0 +1,22 @@
+# Sample file for The Palm Beach Post and related papers. Tweak as you will.
+
+import os
+from multiprocessing import Pool
+
+scraperdir = "../florida-election-results"
+parserdir = "../election-results-parser"
+
+parallelprocesses = ("Florida.py", "PalmBeach.py", "Miami-Dade.py")
+sequentialprocesses = ["composite_csvs.py", "middlewarepre.py", "middlewarepost.py", "app.py fml"]
+
+def run_process(process):
+    os.system('python {}'.format(scraperdir + "/" + process))
+    
+if __name__ ==  '__main__':
+    print("Beginning to scrape.")
+    pool = Pool(processes=8)
+    pool.map(run_process, parallelprocesses)
+    print("Done scraping")
+    os.chdir("../election-results-parser")
+    for script in sequentialprocesses:
+        os.system('python {}'.format(script))
