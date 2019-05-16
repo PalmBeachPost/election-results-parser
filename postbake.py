079589a83e47cd4e543330b3737d27b4300e99e5
Mike Stucka <stucka@whitedoggies.com>
2019-05-16 13:38:44 -0400

    Sync directories pythonically

--- postbake.py
+++ postbake.py
@@ -1,9 +1,15 @@
 import configuration            # local file
 
+from dirsync import sync
+
 import os
 
 webpath = configuration.webpath
 
 os.makedirs(webpath, exist_ok=True)     # Build the directory
-os.system('cp -pr built/* ' + webpath)      # copy files
+
+sync("built/", webpath, "sync", purge=True)
+
+
+# os.system('cp -pr built/* ' + webpath)      # copy files
 # os.system('python snapshotparser.py')
