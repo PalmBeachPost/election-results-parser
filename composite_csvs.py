cb6f8ba6eeeb69433e92932b7b68cb570de2f1e5
Mike Stucka <stucka@whitedoggies.com>
2019-05-16 13:22:37 -0400

    Add robustness to compositor

--- composite_csvs.py
+++ composite_csvs.py
@@ -17,14 +17,17 @@ lineheaders = ["id", "raceid", "racetype", "racetypeid", "ballotorder", "candida
 sourcecsvs = sorted(list(glob.glob(datadir + "*")))
 masterlist = []
 for filename in sourcecsvs:
-    with open(filename, "r") as csvfile:
-        reader = list(csv.DictReader(csvfile))
-    if list(reader[0].keys()) != lineheaders:
-        print("CSV input file " + filename + " has different headers than we're looking for. Not importing.")
-    else:
-        print("CSV input file " + filename + " seems to fit Elex standard. Importing.")
-        for row in reader:
-            masterlist.append(row)
+    try:
+        with open(filename, "r") as csvfile:
+            reader = list(csv.DictReader(csvfile))
+        if list(reader[0].keys()) != lineheaders:
+            print("CSV input file " + filename + " has different headers than we're looking for. Not importing.")
+        else:
+            print("CSV input file " + filename + " seems to fit Elex standard. Importing.")
+            for row in reader:
+                masterlist.append(row)
+    except:
+        print(f"Something weird happened with {filename}.")
 with open(resultscomposite, "w", newline="") as compositefile:
     writer = csv.writer(compositefile)
     writer.writerow(lineheaders)
